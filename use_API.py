e9cc642b28512c02133c72e8bdd05cb50798ec67
zhang3feng888 <130351412+zhang3feng888@users.noreply.github.com>
2025-05-09 13:50:50 +0800

    Create use_API.py

---
+++ use_API.py
@@ -0,0 +1,17 @@
+# 使用API，只有预定好的问题-学挖掘机技术哪家强？
+
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="http://localhost:8000/v1",
+    api_key="sk-xxx", # 随便填写，只是为了通过接口参数校验
+)
+
+chat_outputs = client.chat.completions.create(
+    model="DeepSeek-R1-Distill-Qwen-7B",
+    messages=[
+        {"role": "system", "content": "你是一个职业技术学习专家。"},
+        {"role": "user", "content": "学挖掘机技术哪家强？"},
+    ]
+)
+print(chat_outputs)
